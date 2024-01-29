b500cddce195a33f0b5f0e5c9b047ac517cba171
Jules Schoonman <jules.schoonman@gmail.com>
2024-01-29 21:43:33 +0100

    Squashed commit of the following:
    commit f6c14dd5407341bf6d45fcc712e87f6736dc1df4
    Author: Jules Schoonman <jules.schoonman@gmail.com>
    Date:   Mon Jan 29 21:43:18 2024 +0100
    
        logo marker animation
    
    commit 3fab9247741cd2f2d12edb317e6b4c91ff7f9229
    Author: Jules Schoonman <jules.schoonman@gmail.com>
    Date:   Mon Jan 29 12:37:26 2024 +0100
    
        Added marker in logo
    
    commit 8d2a7922aab24e0661c0a0f51f4bbb6c1b1423da
    Merge: 48fb0a1 9d1ef42
    Author: Jules Schoonman <jules.schoonman@gmail.com>
    Date:   Mon Jan 29 11:27:37 2024 +0100
    
        Merge branch 'main' into dev
    
    commit 48fb0a12196286e656121a61824d4c398930059c
    Author: Jules Schoonman <jules.schoonman@gmail.com>
    Date:   Sun Jan 28 22:53:57 2024 +0100
    
        eyeball wip

--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -1,31 +1,138 @@
 <script lang="ts">
 	import { slideData } from '$lib/shared/stores/markdownSlides.js'
-	import { menu } from '$lib/shared/stores/componentStates.js'
+	import { menu, bear } from '$lib/shared/stores/componentStates.js'
 	import { fade } from 'svelte/transition'
 
 	const toggleMenu = () => menu.toggle()
+
+	// Based on eyeball animation: https://codepen.io/GabEsu/pen/VdKjPE
+
+	let marker: SVGCircleElement | undefined
+	let markerRect: DOMRect | undefined
+	let R: number | undefined
+	let centerX: number | undefined
+	let centerY: number | undefined
+	let angle: number
+	let hover: boolean = false
+	$: transform = `rotate(${angle ? angle : 0})`
+	$: transformInv = `rotate(${angle ? 360 - angle : 0})`
+
+	function setMarker(event: MouseEvent) {
+		if (marker) {
+			const x = event.clientX - centerX
+			const y = event.clientY - centerY
+			const theta = Math.atan2(y, x)
+			angle = (theta * 180) / Math.PI + 360
+		}
+	}
+
+	$: {
+		if (marker) {
+			markerRect = marker.getBoundingClientRect()
+			R = markerRect.width / 2
+			centerX = markerRect.left + R
+			centerY = markerRect.top + R
+		}
+	}
 </script>
 
+<svelte:window on:mousemove={(event) => setMarker(event)} />
+
 <div class="menu" transition:fade>
-	<div class="body">
-		<ul>
-			{#each [...$slideData.keys()] as chapter}
-				<li>
-					<a on:click={toggleMenu} href="#/{chapter}"
-						>{chapter.charAt(0).toUpperCase() + chapter.slice(1)}</a
-					>
-				</li>
-				<ul>
-					{#each [...$slideData.get(chapter).keys()].slice(1) as slideshow}
-						<li>
-							<a on:click={toggleMenu} href="#/{chapter}/{slideshow}/1"
-								>{slideshow.charAt(0).toUpperCase() + slideshow.slice(1)}</a
-							>
-						</li>
-					{/each}
-				</ul>
-			{/each}
-		</ul>
+	<div class="container">
+		<div class="logo">
+			<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 180 249">
+				<g fill="rgb(252,217,213)" fill-rule="nonzero" id="group_artwork">
+					<path
+						id="body"
+						d="M137.8,108c15.6-8.5,26.2-25,26.2-44c0-27.6-22.4-50-50-50H14v94H0v127h180V108H137.8z M114,221H14V121h100
+        c27.6,0,50,22.4,50,50S141.6,221,114,221z"
+					/>
+				</g>
+				<g stroke="rgb(119, 63, 63)" fill="none" stroke-width="6">
+					<circle
+						class="circle"
+						class:circle-transform-cw={hover}
+						bind:this={marker}
+						cx="127"
+						cy="64"
+						r="27"
+						stroke-dasharray="4.71238898038469"
+						stroke-dashoffset="4.71238898038469"
+						{transform}
+					/>
+					<circle
+						class="circle"
+						class:circle-transform-ccw={hover}
+						cx="127"
+						cy="64"
+						r="21"
+						stroke-dasharray="3.6651914291880923"
+						stroke-dashoffset="0"
+						transform={transformInv}
+					/>
+					<circle
+						class="circle"
+						class:circle-transform-cw={hover}
+						cx="127"
+						cy="64"
+						r="15"
+						stroke-dasharray="2.617993877991494"
+						stroke-dashoffset="2.617993877991494"
+						{transform}
+					/>
+					<circle
+						class="circle"
+						class:circle-transform-ccw={hover}
+						cx="127"
+						cy="64"
+						r="9"
+						stroke-dasharray="1.5707963267948966"
+						stroke-dashoffset="0"
+						stroke-width="6"
+						transform={transformInv}
+					/>
+					<circle
+						class="circle"
+						class:circle-transform-cw={hover}
+						cx="127"
+						cy="64"
+						r="3"
+						stroke-dasharray="0.5235987755982988"
+						stroke-dashoffset="0.5235987755982988"
+						{transform}
+					/>
+				</g>
+				{#if $bear}{/if}
+			</svg>
+		</div>
+		<div class="menu-items">
+			<ul class="chapters">
+				{#each [...$slideData.keys()] as chapter}
+					<li>
+						<a
+							on:click={toggleMenu}
+							on:mouseenter={() => (hover = true)}
+							on:mouseleave={() => (hover = false)}
+							href="#/{chapter}">{chapter.charAt(0).toUpperCase() + chapter.slice(1)}</a
+						>
+					</li>
+					<ul class="slideshows">
+						{#each [...$slideData.get(chapter).keys()].slice(1) as slideshow}
+							<li>
+								<a
+									on:click={toggleMenu}
+									on:mouseenter={() => (hover = true)}
+									on:mouseleave={() => (hover = false)}
+									href="#/{chapter}/{slideshow}/1"
+									>{slideshow.charAt(0).toUpperCase() + slideshow.slice(1)}</a
+								>
+							</li>
+						{/each}
+					</ul>
+				{/each}
+			</ul>
+		</div>
 	</div>
 </div>
 
@@ -33,19 +140,21 @@
 	.menu {
 		grid-column: 1 / 5;
 		grid-row: 1 / 3;
-		background-color: rgba(0, 0, 0, 0.6);
-		padding-left: 20px;
-		padding-right: 20px;
-		padding-top: 3rem;
+		background-color: rgba(0, 0, 0, 0.7);
+		padding: 3rem 1rem 1rem 1rem;
 		min-width: 0;
 		min-height: 0;
 		overflow: auto;
 		z-index: 5;
 		display: flex;
-		justify-content: center;
-		align-items: center;
 		line-height: 1.3;
 		color: white;
+	}
+	.container {
+		display: flex;
+		flex-wrap: wrap;
+		width: 100%;
+		height: 100%;
 	}
 	a {
 		color: white;
@@ -55,23 +164,74 @@
 		color: rgba(255, 255, 114);
 		text-decoration: none;
 	}
-	.body {
-		font-size: 1.2rem;
-		max-width: 700px;
-		max-height: 100%;
+	.logo {
+		order: 1;
+		height: 100%;
+		width: 50%;
+		& svg {
+			width: 100%;
+			height: 100%;
+		}
 	}
-	ul {
-		margin-left: 0;
-		padding-left: 1em;
-		padding-bottom: 1em;
+	.menu-items {
+		order: 2;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+		width: 50%;
+		margin: 0;
+		font-size: 1.2rem;
+	}
+	.circle {
+		transform-origin: 127px 64px;
+	}
+	@keyframes rotation {
+		0% {
+			transform: rotate(-360deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+	@keyframes rotation-ccw {
+		0% {
+			transform: rotate(360deg);
+		}
+		100% {
+			transform: rotate(-360deg);
+		}
+	}
+	.circle-transform-cw {
+		animation: rotation 8s linear infinite;
+	}
+	.circle-transform-ccw {
+		animation: rotation-ccw 8s linear infinite;
+	}
+	ul.chapters {
+		margin: 0;
+		padding: 0;
+		list-style-type: none;
+	}
+	ul.slideshows {
+		margin: 0;
+		padding-bottom: 1rem;
+		padding-left: 1rem;
 		list-style-type: none;
 	}
 	@media all and (max-width: 700px) {
-		.body {
-			height: 100%;
+		ul.chapters {
+			margin-top: 1rem;
 		}
-		.html {
-			columns: 1;
+		.menu-items {
+			order: 1;
+			height: auto;
+			width: 100%;
+		}
+		.logo {
+			order: 2;
+			height: auto;
+			width: 100%;
 		}
 	}
 </style>
